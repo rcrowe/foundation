83e5a7f1535761ac98dd4386691fea2c760243c2
Taylor Otwell <taylorotwell@gmail.com>
2012-06-28 13:20:39 -0500

    change location of functions.

--- src/Illuminate/Foundation/LightSwitch.php
+++ src/Illuminate/Foundation/LightSwitch.php
@@ -21,7 +21,7 @@ class LightSwitch {
 
 		Helpers::register();
 
-		require_once __DIR__.'/Functions.php';
+		require_once __DIR__.'/../../helpers.php';
 
 		// We will go ahead and create the Illuminate application since there
 		// are no constructor arguments that are needed and we want to put
