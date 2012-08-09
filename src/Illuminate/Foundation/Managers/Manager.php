14234666fbdae58305daf6bb2abf383cae24a8f8
Taylor Otwell <taylorotwell@gmail.com>
2012-08-08 22:00:30 -0500

    added __call method to default manager.

--- src/Illuminate/Foundation/Managers/Manager.php
+++ src/Illuminate/Foundation/Managers/Manager.php
@@ -95,4 +95,16 @@ abstract class Manager {
 		return $this->drivers;
 	}
 
+	/**
+	 * Dynamically call the default driver instance.
+	 *
+	 * @param  string  $method
+	 * @param  array   $parameters
+	 * @return mixed
+	 */
+	public function __call($method, $parameters)
+	{
+		return call_user_func_array($this->driver(), $parameters);
+	}
+
 }
