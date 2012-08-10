2887349ba3f76e6af4b8c111ec7d091e9f43cfc8
Taylor Otwell <taylorotwell@gmail.com>
2012-08-09 23:49:55 -0500

    register the memcached connector.

--- src/Illuminate/Foundation/Providers/CacheServiceProvider.php
+++ src/Illuminate/Foundation/Providers/CacheServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace Illuminate\Foundation\Providers;
 
 use Illuminate\Foundation\Application;
+use Illuminate\Cache\MemcachedConnector;
 use Illuminate\Foundation\Managers\CacheManager;
 
 class CacheServiceProvider extends ServiceProvider {
@@ -16,6 +17,11 @@ class CacheServiceProvider extends ServiceProvider {
 		$app['cache'] = $app->share(function($app)
 		{
 			return new CacheManager($app);
+		});
+
+		$app['memcached.connector'] = $app->share(function()
+		{
+			return new MemcachedConnector;
 		});
 	}
 
