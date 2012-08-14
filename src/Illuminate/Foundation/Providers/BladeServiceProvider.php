dd0d081227806901dbb7f8d68909483646ee8110
Taylor Otwell <taylorotwell@gmail.com>
2012-08-13 23:39:08 -0500

    cleaning up the blade provider.

--- src/Illuminate/Foundation/Providers/BladeServiceProvider.php
+++ src/Illuminate/Foundation/Providers/BladeServiceProvider.php
@@ -17,20 +17,40 @@ class BladeServiceProvider extends ServiceProvider {
 	 */
 	public function register(Application $app)
 	{
+		$this->registerLoader($app);
+
+		$this->registerEnvironment($app);
+	}
+
+	/**
+	 * Register the Blade loader with the application.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	protected function registerLoader($app)
+	{
+		// First we need to setup the paths for the Blade engine. These are simply the
+		// storage path for these templates, as well as the path where the compiled
+		// templates are stored for fast rendering. Then we'll create the loader.
 		$app['blade.path'] = $app['path'].'/views';
 
 		$app['blade.cache'] = $app['blade.path'].'/cache';
 
 		$app['blade.loader'] = $app->share(function($app)
 		{
-			// We'll create a Blade loader instance with the path and cache paths set on
-			// the application. The loader is responsible for actually returning the
-			// fully qualified paths to the blade views to the factory instances.
-			$loader = new Loader(new Compiler, new Filesystem, $app['blade.path'], $app['blade.cache']);
-
-			return $loader;
+			return Loader::newInstance($app['blade.path'], $app['blade.cache']);
 		});
+	}
 
+	/**
+	 * Register the Blade environment instance.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	protected function registerEnvironment($app)
+	{
 		$app['blade'] = $app->share(function($app)
 		{
 			$blade = new Environment($app['blade.loader']);
@@ -42,8 +62,14 @@ class BladeServiceProvider extends ServiceProvider {
 
 			if (isset($app['session']) and $app['session']->has('errors'))
 			{
-				$blade->share('errors', $app['session']->get('errors'));
+				$errors = $app['session']->get('errors');
+
+				$blade->share('errors', $errors);
 			}
+
+			// Even if no error object is present in the session, we will still bind an
+			// instance onto the template simply so all views can assume that one is
+			// available to them. This avoids the need for a lot of pointless ifs.
 			else
 			{
 				$blade->share('errors', new MessageBag);
