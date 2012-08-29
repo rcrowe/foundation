379b748960260db433a6a188ff33f0fa25b02b2b
Taylor Otwell <taylorotwell@gmail.com>
2012-08-28 23:07:26 -0500

    work on database service provider.

--- src/Illuminate/Foundation/Providers/DatabaseServiceProvider.php
+++ src/Illuminate/Foundation/Providers/DatabaseServiceProvider.php
@@ -1,6 +1,7 @@
 <?php namespace Illuminate\Foundation\Providers;
 
 use Illuminate\Foundation\Application;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Managers\DatabaseManager;
 use Illuminate\Database\Connectors\ConnectionFactory;
 
@@ -14,6 +15,11 @@ class DatabaseServiceProvider extends ServiceProvider {
 	 */
 	public function register(Application $app)
 	{
+		$this->registerEloquent($app);
+
+		// The connection factory is used to create the actual connection instances on
+		// the database. We will inject the factory into the manager so that it may
+		// make the connections while they are actually needed and not of before.
 		$app['db.factory'] = $app->share(function()
 		{
 			return new ConnectionFactory;
@@ -23,6 +29,30 @@ class DatabaseServiceProvider extends ServiceProvider {
 		{
 			return new DatabaseManager($app, $app['db.factory']);
 		});
+	}
+
+	/**
+	 * Register the database connections with the Eloquent ORM.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	protected function registerEloquent(Application $app)
+	{
+		$connections = array_keys($app['config']['database.connections']);
+
+		// To setup the Eloquent ORM, we will register a resolver for each connection
+		// that is configured for the application. We'll defer the creation of any
+		// of the connections using a Closure which gets resolved when executed.
+		foreach ($connections as $name)
+		{
+			Model::addConnection($connection, function() use ($app, $name)
+			{
+				return $app['db']->connection($name);
+			})
+		}
+
+		Model::setDefaultConnectionName($app['config']['database.default']);
 	}
 
 }
