e8fa8dea81c248cc907826e50ec28c9eaa24c7ef
Taylor Otwell <taylorotwell@gmail.com>
2012-10-16 19:12:25 -0500

    added controller service provider.

---
+++ src/Illuminate/Foundation/Providers/ControllerServiceProvider.php
@@ -0,0 +1,62 @@
+<?php namespace Illuminate\Foundation\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Controllers\FilterParser;
+use Doctrine\Common\Annotations\SimpleAnnotationReader;
+
+class ControllerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if the service provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	public function register($app)
+	{
+		$this->registerReader($app);
+
+		$app['filter.parser'] = $app->share(function($app)
+		{
+			$path = $app['path'].'/storage/meta';
+
+			return new FilterParser($app['annotation.reader'], $app['files'], $path);
+		});
+	}
+
+	/**
+	 * Register the annotation reader.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	protected function registerReader($app)
+	{
+		$app['annotation.reader'] = $app->share(function()
+		{
+			$reader = new SimpleAnnotationReader;
+
+			$reader->addNamespace('Illuminate\Routing\Controllers');
+
+			return $reader;
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function getProvidedServices()
+	{
+		return array('filter.parser');
+	}
+
+}
