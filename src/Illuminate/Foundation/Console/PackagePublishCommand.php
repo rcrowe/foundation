30914e57d1a151e83bac545e7f9e7584e9e48b15
Taylor Otwell <taylorotwell@gmail.com>
2012-10-27 22:46:08 -0500

    add publish command and tests.

---
+++ src/Illuminate/Foundation/Console/PackagePublishCommand.php
@@ -0,0 +1,69 @@
+<?php namespace Illuminate\Foundation\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Foundation\AssetPublisher;
+use Symfony\Component\Console\Input\InputArgument;
+
+class PackagePublishCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'package:publish';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = "Publish a package assets to the public directory";
+
+	/**
+	 * The asset publisher instance.
+	 *
+	 * @var Illuminate\Foundation\AssetPublisher
+	 */
+	protected $assets;
+
+	/**
+	 * Create a new package publish command instance.
+	 *
+	 * @param  Illuminate\Foundation\AssetPublisher  $assets
+	 * @return void
+	 */
+	public function __construct(AssetPublisher $assets)
+	{
+		parent::__construct();
+
+		$this->assets = $assets;
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return void
+	 */
+	public function fire()
+	{
+		$package = $this->input->getArgument('package');
+
+		$this->assets->publishPackage($package);
+
+		$this->output->writeln('<info>Assets published for package:</info> '.$package);
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array(
+			array('package', InputArgument::REQUIRED, 'The name of package to publish'),
+		);
+	}
+
+}
