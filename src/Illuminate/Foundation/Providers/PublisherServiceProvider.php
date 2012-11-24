b581304022986621636b3c655807ff1f69e56d8a
Taylor Otwell <taylorotwell@gmail.com>
2012-11-24 13:11:05 -0600

    update publisher service provider.

--- src/Illuminate/Foundation/Providers/PublisherServiceProvider.php
+++ src/Illuminate/Foundation/Providers/PublisherServiceProvider.php
@@ -14,11 +14,29 @@ class PublisherServiceProvider extends ServiceProvider {
 	 */
 	public function register($app)
 	{
+		$this->registerAssetPublisher($app);
+
+		$this->registerConfigPublisher($app);
+	}
+
+	/**
+	 * Register the asset publisher service and command.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	protected function registerAssetPublisher($app)
+	{
 		$this->registerAssetPublishCommand($app);
 
 		$app['asset.publisher'] = $app->share(function($app)
 		{
-			$publisher = new AssetPublisher($app['files'], $app['path.base'].'/public');
+			$publicPath = $app['path.base'].'/public';
+
+			// The asset "publisher" is responsible for moving package's assets into the
+			// web accessible public directory of an application so they can actually
+			// be served to the browser. Otherwise, they would be locked in vendor.
+			$publisher = new AssetPublisher($app['files'], $publicPath);
 
 			$publisher->setPackagePath($app['path.base'].'/vendor');
 
@@ -37,6 +55,45 @@ class PublisherServiceProvider extends ServiceProvider {
 		$app['command.asset.publish'] = $app->share(function($app)
 		{
 			return new AssetPublishCommand($app['asset.publisher']);
+		});
+	}
+
+	/**
+	 * Register the configuration publisher class and command.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	protected function registerConfigPublisher($app)
+	{
+		$this->registerConfigPublishCommand($app);
+
+		$app['config.publisher'] = $app->share(function($app)
+		{
+			$configPath = $app['path'].'/config';
+
+			// Once we have created the configuration publisher, we will set the default
+			// package path on the object so that it knows where to find the packages
+			// that are installed for the application and can move them to the app.
+			$publisher = new ConfigPublisher($app['files'], $configPath);
+
+			$publisher->setPackagePath($app['path.base'].'/vendor');
+
+			return $publisher;
+		});
+	}
+
+	/**
+	 * Register the configuration publish console command.
+	 *
+	 * @param  Illuminate\Foundation\Application  $app
+	 * @return void
+	 */
+	protected function registerConfigPublishCommand($app)
+	{
+		$app['command.config.publish'] = $app->share(function($app)
+		{
+			return new ConfigPublishCommand($app['config.publisher']);
 		});
 	}
 
